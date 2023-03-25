0eae09d4115f6d40de5a163377038ff3611da809
suraj chauhan <sonubintw@gmail.com>
2023-03-25 12:36:40 +0530

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+// var num1=10;
+// var num2=10;
+// console.log(num1+num2);
+
+// function sui(a) {
+//     per=(7.1 * a + 11);
+//     return per;
+// }
+// c = sui(7.5);
+
+// console.log(c);
+
+// var age=17;
+// if (age>18){
+//     console.log('you are an adult')
+// }
+// else{
+//     console.log('stfu kid')
+// }
+
+// var suii=[1,2,3,4,5];
+// for(var i=0;i< (suii.length);i++)
+// {
+//     console.log(suii[i])
+// }
+// suii.pop();
+// console.log(suii)
+
+// let mydate=Date()
+// console.log(mydate)
+// mydate.get
+
+let s=document.getElementsByClassName("row")
+console.log(s)
